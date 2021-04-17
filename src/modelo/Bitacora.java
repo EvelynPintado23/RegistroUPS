c2b1682a058f5d106946fb40a02bef314e2ec535
EvelynPintado23 <epintadoc@est.ups.edu.ec>
2021-04-17 18:35:36 -0500

    Creacion de DAOs

--- src/modelo/Bitacora.java
+++ src/modelo/Bitacora.java
@@ -1,5 +1,23 @@
 package modelo;
 
 public class Bitacora {
+	
+	private int id_laboratorio;
+	private int bitacora_num_ediciones;
 
+	
+	public int getId_laboratorio() {
+		return id_laboratorio;
+	}
+	public void setId_laboratorio(int id_laboratorio) {
+		this.id_laboratorio = id_laboratorio;
+	}
+	public int getBitacora_num_ediciones() {
+		return bitacora_num_ediciones;
+	}
+	public void setBitacora_num_ediciones(int bitacora_num_ediciones) {
+		this.bitacora_num_ediciones = bitacora_num_ediciones;
+	}
+	
 }
+
